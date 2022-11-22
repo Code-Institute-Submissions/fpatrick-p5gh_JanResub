197abe36e2c0659c5e0df879f22c0f491678bfe3
erick <fpatrick.erick@gmail.com>
2022-11-22 12:52:25 +0000

    add category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from . models import Product
+from . models import Product, Category
 # Create your views here.
 
 
@@ -11,8 +11,14 @@ def all_products(request):
     products = Product.objects.all()
     # To not get an error when loading page without a search term
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         # Use Q to match term of product name OR (instead of and) description
         if 'q' in request.GET:
             query = request.GET['q']
@@ -26,6 +32,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
