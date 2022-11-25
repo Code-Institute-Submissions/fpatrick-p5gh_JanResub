e288fd7fe0105e3f54e3d6ac8c3e268522083756
erick <fpatrick.erick@gmail.com>
2022-11-25 16:11:31 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
-
+# test key pk_test_51M84AgAYOJ9b3UAu2CsHcVDTHR4qOAzv6UxJWlY4NdhgYCNLWgbESeyF5ClxdIdrCSGwx4FDOEZD6JkZUxt8VJ6s00EMnRW9vV
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51M84AgAYOJ9b3UAu2CsHcVDTHR4qOAzv6UxJWlY4NdhgYCNLWgbESeyF5ClxdIdrCSGwx4FDOEZD6JkZUxt8VJ6s00EMnRW9vV',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
