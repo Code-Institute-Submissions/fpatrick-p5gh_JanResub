f3fe8332f9de8a4b8313b9dbe5397ed2348de60e
erick <fpatrick.erick@gmail.com>
2022-11-25 16:38:20 +0000

    add basic checkout functionality

--- p5gh/settings.py
+++ p5gh/settings.py
@@ -178,3 +178,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # To calculate delivery costs
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
