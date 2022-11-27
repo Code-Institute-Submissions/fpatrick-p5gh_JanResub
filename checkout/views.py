1c9efd8fc5daa8a4ea45538c9f426d8e0df19a17
erick <fpatrick.erick@gmail.com>
2022-11-27 15:23:27 +0000

    add stock quantity functionality

--- checkout/views.py
+++ checkout/views.py
@@ -64,22 +64,31 @@ def checkout(request):
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
+                    if product.stock >= item_data:
+                        if isinstance(item_data, int):
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
-                                quantity=quantity,
-                                product_size=size,
+                                quantity=item_data,
                             )
                             order_line_item.save()
+                        else:
+                            for size, quantity in item_data['items_by_size'].items():
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    product=product,
+                                    quantity=quantity,
+                                    product_size=size,
+                                )
+                                order_line_item.save()
+                    else:
+                        messages.error(request, (
+                            "One of the products in your don't have enough stock. "
+                            "Please check your bag or call us for assistance!")
+                                       )
+                        order.delete()
+                        return redirect(reverse('view_bag'))
+
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag wasn't "
