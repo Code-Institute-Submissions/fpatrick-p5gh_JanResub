f7e3468290e77445836fa7be41e70bab4caaaa2b
erick <fpatrick.erick@gmail.com>
2022-11-28 09:41:14 +0000

    remove secret key and debug

--- p5gh/settings.py
+++ p5gh/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%z8+^u2n&_^u0sc^w%bo=gqmqjr1pf8!hn^2##%4w*af+t^2u2'
+SECRET_KEY = os.environ.get('SECRET_key', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gamerhood.herokuapp.com', 'localhost', '127.0.0.1']
 
