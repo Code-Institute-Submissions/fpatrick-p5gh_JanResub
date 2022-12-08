28f3c5dfd490e6067396185f1d2050014b67707a
erick <fpatrick.erick@gmail.com>
2022-12-08 13:20:34 +0000

    change home and navbar to dark colors and round style

--- p5gh/settings.py
+++ p5gh/settings.py
@@ -22,10 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '23432kjh32kjdhjkdhkdjh2k323'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEBUG'
 
 ALLOWED_HOSTS = ['gamerhood.herokuapp.com', 'localhost', '127.0.0.1']
 
