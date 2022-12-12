f8e01b29f9a94fa57f9bd10d73d08c23e5e9ab71
erick <fpatrick.erick@gmail.com>
2022-12-12 15:44:18 +0000

    change settings to run on heroku production mode

--- p5gh/settings.py
+++ p5gh/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '23432kjh32kjdhjkdhkdjh2k323'
-DEBUG = 'DEBUG'
+#SECRET_KEY = '23432kjh32kjdhjkdhkdjh2k323'
+#DEBUG = 'DEBUG'
 
-#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
+
+
+if not SECRET_KEY:
+    SECRET_KEY = '23432kjh32kjdhjkdhkdjh2k323'
+    DEBUG = 'DEBUG'
 
 
 ALLOWED_HOSTS = ['gamerhood.herokuapp.com', 'localhost', '127.0.0.1']
