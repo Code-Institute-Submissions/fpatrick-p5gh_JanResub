82a74203ea45a0fef5637ee404267d611af6df11
erick <fpatrick.erick@gmail.com>
2023-01-13 12:51:14 +0000

    add to testimonial creation functionality based on user

--- p5gh/urls.py
+++ p5gh/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
+    path('testimonials/', include('testimonials.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'p5gh.views.handler404'
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import UpdateTestimonialForm, TestimonialForm
+from .models import Testimonial
+
 
 # Create your views here.
+def testimonials(request):
+    testimonials = Testimonial.objects.all()
+    return render(request, 'testimonials.html', {'testimonials': testimonials})
+
+
+@login_required
+def view_testimonial(request):
+    try:
+        testimonial = Testimonial.objects.get(user=request.user)
+        return render(request, 'view_testimonial.html', {'testimonial': testimonial})
+    except Testimonial.DoesNotExist:
+        return redirect('add_testimonial')
+
+
+@login_required
+def add_testimonial(request):
+    try:
+        testimonial = Testimonial.objects.get(user=request.user)
+        return render(request, 'view_testimonial.html', {'testimonial': testimonial})
+    except Testimonial.DoesNotExist:
+        if request.method == 'POST':
+            form = TestimonialForm(request.POST)
+            if form.is_valid():
+                testimonial = form.save(commit=False)
+                testimonial.user_id = request.user.id
+                testimonial.save()
+                messages.success(request, 'Testimonial created successfully')
+                return redirect('view_testimonial')
+        else:
+            form = TestimonialForm()
+        return render(request, 'add_testimonial.html', {'form': form})
+
