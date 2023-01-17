181b04e6f2c7909cb50e98abe000dba7a64ebbeb
erick <fpatrick.erick@gmail.com>
2023-01-17 16:51:24 +0000

    add functionality to grab latest 6 testimonials and display on homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from testimonials.models import Testimonial
+
+
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    latest_testimonials = Testimonial.objects.all().order_by('-created_at')[:3]
+    more_testimonials = Testimonial.objects.all().order_by('-created_at')[3:6]
+    return render(request, 'home/index.html', {'latest_testimonials': latest_testimonials,
+                                               'more_testimonials': more_testimonials})
